f74d471f1b31fbadcb1ce4700334b0f71e66f24a
Julia Krause <if14b055@technikum-wien.at>
2016-12-15 17:23:57 +0100

    MovieClient without resteasy and with filename as command line argument, should work

--- src/at/technikumwien/MovieClient.java
+++ src/at/technikumwien/MovieClient.java
@@ -20,8 +20,7 @@ public class MovieClient {
 
     public static void main ( String [] args ) throws Exception {
 
-        //Versuch, über Shell xmlFileNamen einzugeben
-        /*System.out.println("Eingabe: ");
+        System.out.println("Filename is: ");
 
         if(args.length > 0) {
             for(String arg : args) {
@@ -29,11 +28,9 @@ public class MovieClient {
             }
         } else {
             System.out.println("No args to print");
-        }*/
+        }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter String");
-        final String XML_FILE = br.readLine();
+        final String XML_FILE = args[0];
 
         MovieWebService_Service service = new MovieWebService_Service();
         MovieWebService port = service.getMovieWebServicePort();
@@ -46,26 +43,14 @@ public class MovieClient {
         JAXBElement<MovieList> jaxbElement = unmarshaller.unmarshal(source, MovieList.class);
         MovieList movieListWrapper = jaxbElement.getValue();
         List<Movie> movieList = movieListWrapper.getMovie();
-        System.out.println(movieList.get(0).getActors().getActor().get(0).getBirthdate());
+        //System.out.println(movieList.get(0).getActors().getActor().get(0).getBirthdate());
 
         //senden an WebService
-        //at first, server said: illegalArgumentException thrown by importMovies(), something about Gregorian Calendar
         //had to change type of BirthDate in generated entity Actor (hope that's okay)
         //for some reason, wsimport made this a GregorianCalendar type, but we want Date
-        //now it's working!!!
         //man könnte das Ganze natürlich auch anders lösen
-        //entweder am Server bei Actor auch GregorianCalender als Type für BirthDate nehmen
-        //(ich nehme an, dann wäre das in der WSDL auch so angegeben und würde von hier aus gehen)
-        //oder halt am Client einen Converter machen, der für alle Schauspieler den Typ von BirthDate
-        //von GregorianCalender in Date umwandelt
 
         port.importMovies(movieListWrapper);
-
-        //hier hab ich mal was probiert, weil irgendwozu ja die ImportMovies Klasse gut sein muss,
-        // weiß nicht, ob dabei jetzt was anderes rauskommen würde
-        /*ImportMovies importMovies = new ImportMovies();
-        importMovies.setMovies(movieListWrapper);
-        port.importMovies(importMovies.getMovies());*/
 
     }
 
